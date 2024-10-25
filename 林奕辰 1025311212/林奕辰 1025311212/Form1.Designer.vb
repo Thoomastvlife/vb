ffcd91b23d87235f9f243d6d68b068b0aed1b7ab
Thoomastvlife <153334866+Thoomastvlife@users.noreply.github.com>
2024-10-25 15:24:09 +0800

    Add files via upload

---
+++ 林奕辰 1025311212/林奕辰 1025311212/Form1.Designer.vb
@@ -0,0 +1,171 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        btn_01 = New Button()
+        btn_02 = New Button()
+        btn_03 = New Button()
+        btn_05 = New Button()
+        btn_04 = New Button()
+        btn_print = New Button()
+        pd = New Printing.PrintDocument()
+        txt_01 = New TextBox()
+        txt_02 = New TextBox()
+        txt_03 = New TextBox()
+        txt_04 = New TextBox()
+        txt_05 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' btn_01
+        ' 
+        btn_01.Location = New Point(33, 82)
+        btn_01.Name = "btn_01"
+        btn_01.Size = New Size(111, 47)
+        btn_01.TabIndex = 0
+        btn_01.Text = "第一題"
+        btn_01.UseVisualStyleBackColor = True
+        ' 
+        ' btn_02
+        ' 
+        btn_02.Location = New Point(279, 82)
+        btn_02.Name = "btn_02"
+        btn_02.Size = New Size(102, 47)
+        btn_02.TabIndex = 1
+        btn_02.Text = "第二題"
+        btn_02.UseVisualStyleBackColor = True
+        ' 
+        ' btn_03
+        ' 
+        btn_03.Location = New Point(523, 82)
+        btn_03.Name = "btn_03"
+        btn_03.Size = New Size(129, 47)
+        btn_03.TabIndex = 2
+        btn_03.Text = "第三題"
+        btn_03.UseVisualStyleBackColor = True
+        ' 
+        ' btn_05
+        ' 
+        btn_05.Location = New Point(1003, 82)
+        btn_05.Name = "btn_05"
+        btn_05.Size = New Size(113, 47)
+        btn_05.TabIndex = 3
+        btn_05.Text = "第五題"
+        btn_05.UseVisualStyleBackColor = True
+        ' 
+        ' btn_04
+        ' 
+        btn_04.Location = New Point(799, 82)
+        btn_04.Name = "btn_04"
+        btn_04.Size = New Size(116, 47)
+        btn_04.TabIndex = 4
+        btn_04.Text = "第四題"
+        btn_04.UseVisualStyleBackColor = True
+        ' 
+        ' btn_print
+        ' 
+        btn_print.Location = New Point(1107, 12)
+        btn_print.Name = "btn_print"
+        btn_print.Size = New Size(131, 52)
+        btn_print.TabIndex = 5
+        btn_print.Text = "列印答案"
+        btn_print.UseVisualStyleBackColor = True
+        ' 
+        ' pd
+        ' 
+        ' 
+        ' txt_01
+        ' 
+        txt_01.Location = New Point(23, 167)
+        txt_01.Multiline = True
+        txt_01.Name = "txt_01"
+        txt_01.Size = New Size(238, 293)
+        txt_01.TabIndex = 6
+        ' 
+        ' txt_02
+        ' 
+        txt_02.Location = New Point(279, 167)
+        txt_02.Multiline = True
+        txt_02.Name = "txt_02"
+        txt_02.Size = New Size(238, 293)
+        txt_02.TabIndex = 7
+        ' 
+        ' txt_03
+        ' 
+        txt_03.Location = New Point(523, 167)
+        txt_03.Multiline = True
+        txt_03.Name = "txt_03"
+        txt_03.Size = New Size(270, 258)
+        txt_03.TabIndex = 8
+        ' 
+        ' txt_04
+        ' 
+        txt_04.Location = New Point(799, 167)
+        txt_04.Multiline = True
+        txt_04.Name = "txt_04"
+        txt_04.Size = New Size(198, 293)
+        txt_04.TabIndex = 9
+        ' 
+        ' txt_05
+        ' 
+        txt_05.Location = New Point(1003, 167)
+        txt_05.Multiline = True
+        txt_05.Name = "txt_05"
+        txt_05.Size = New Size(315, 258)
+        txt_05.TabIndex = 10
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1341, 592)
+        Controls.Add(txt_05)
+        Controls.Add(txt_04)
+        Controls.Add(txt_03)
+        Controls.Add(txt_02)
+        Controls.Add(txt_01)
+        Controls.Add(btn_print)
+        Controls.Add(btn_04)
+        Controls.Add(btn_05)
+        Controls.Add(btn_03)
+        Controls.Add(btn_02)
+        Controls.Add(btn_01)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents btn_01 As Button
+    Friend WithEvents btn_02 As Button
+    Friend WithEvents btn_03 As Button
+    Friend WithEvents btn_05 As Button
+    Friend WithEvents btn_04 As Button
+    Friend WithEvents btn_print As Button
+    Friend WithEvents pd As Printing.PrintDocument
+    Friend WithEvents txt_01 As TextBox
+    Friend WithEvents txt_02 As TextBox
+    Friend WithEvents txt_03 As TextBox
+    Friend WithEvents txt_04 As TextBox
+    Friend WithEvents txt_05 As TextBox
+
+End Class
---
+++ 林奕辰 1025311212/林奕辰 1025311212/Form1.vb
@@ -0,0 +1,147 @@
+﻿Public Class Form1
+
+    '*****************************
+    '* 1990-1060301 Program Start *
+    '*****************************
+
+    Private Sub btn_01_Click(sender As Object, e As EventArgs) Handles btn_01.Click
+        Dim m0, m1, m2, m3, m4
+
+        FileOpen(1, "c:\test\1060301.sm", OpenMode.Input)
+        Input(1, m0)
+        FileClose(1)
+
+        m1 = m0
+        For i = 1 To 10
+            m2 = m1 \ 10
+            m3 = m1 Mod 10
+            m4 = m4 & m3
+            If m2 = 0 Then
+                Exit For
+            End If
+            m1 = m2
+
+        Next
+        If m4 = m0 Then
+            txt_01.Text = "第一題結果 : " & m0 & "is a palindrome."
+        Else
+            txt_01.Text = "第一題結果 : " & m0 & "is not a palindrome."
+        End If
+    End Sub
+
+    '*****************************
+    '* 1990-1060302 Program Start *
+    '*****************************
+
+    Private Sub btn_02_Click(sender As Object, e As EventArgs) Handles btn_02.Click
+        Dim m0, m1, m2, m3, m4
+
+        FileOpen(1, "c:\test\1060302.sm", OpenMode.Input)
+        Input(1, m0)
+        FileClose(1)
+
+        For i = 1 To m0
+            For j = 1 To i
+                m1 = m1 & j
+            Next
+            m1 = m1 & vbCrLf
+        Next
+        txt_02.Text = "第二題結果 : " & vbCrLf & vbCrLf & m1
+
+    End Sub
+
+    '*****************************
+    '* 1990-1060303 Program Start *
+    '*****************************
+
+    Private Sub btn_03_Click(sender As Object, e As EventArgs) Handles btn_03.Click
+        Dim m0, m1, m2, m3, m4
+
+        FileOpen(1, "c:\test\1060303.sm", OpenMode.Input)
+        Input(1, m0)
+        FileClose(1)
+
+        For i = 1 To m0
+            If m0 Mod i = 0 Then m1 = m1 + 1
+
+        Next
+        If m1 = 2 Then
+            txt_03.Text = "第三題結果 : " & m0 & "is a prime number."
+        Else
+            txt_03.Text = "第三題結果 : " & m0 & "is not a prime number."
+        End If
+    End Sub
+
+    '*****************************
+    '* 1990-1060304 Program Start *
+    '*****************************
+
+    Private Sub btn_04_Click(sender As Object, e As EventArgs) Handles btn_04.Click
+        Dim h(3), w(3), bmi(3), sbmi
+
+        FileOpen(1, "c:\test\1060304.sm", OpenMode.Input)
+
+
+        For i = 1 To 3
+            Input(1, h(i))
+            Input(1, w(i))
+            bmi(i) = w(i) / (h(i) / 100) ^ 2
+
+        Next
+        FileClose(1)
+        sbmi = bmi(1)
+        For i = 1 To 3
+            If bmi(i) < sbmi Then sbmi = bmi(i)
+        Next
+        Dim sbmi45 As Integer = sbmi
+        If sbmi45 <= 25 And sbmi45 >= 20 Then
+            txt_04.Text = "第四題結果 : 最小BMI值" & sbmi45 & "正常"
+        Else
+            txt_04.Text = "第四題結果 : 最小BMI值" & sbmi45 & "不正常"
+
+        End If
+    End Sub
+
+    '*****************************
+    '* 1990-1060305 Program Start *
+    '*****************************
+
+    Private Sub btn_05_Click(sender As Object, e As EventArgs) Handles btn_05.Click
+        Dim a(2, 2), b(2, 2), c(2, 2)
+
+        FileOpen(1, "c:\test\1060305.sm", OpenMode.Input)
+        Input(1, a(1, 1)) : Input(1, a(1, 2))
+        Input(1, a(2, 1)) : Input(1, a(2, 2))
+        Input(1, b(1, 1)) : Input(1, b(1, 2))
+        Input(1, b(2, 1)) : Input(1, b(2, 2))
+
+        FileClose(1)
+        For i = 1 To 2
+            For j = 1 To 2
+                c(i, j) = a(i, j) + b(i, j)
+
+            Next
+        Next
+        Dim ans As String
+        ans &= "[" & c(1, 1) & vbTab & c(1, 2) & "]" & vbCrLf
+        ans &= "[" & c(2, 1) & vbTab & c(2, 2) & "]" & vbCrLf
+        txt_05.Text = "第五題結果 :" & vbCrLf & ans
+    End Sub
+
+
+    Private Sub btn_print_Click(sender As Object, e As EventArgs) Handles btn_print.Click
+        pd.Print()
+    End Sub
+
+    Private Sub pd_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles pd.PrintPage
+        Dim ans As String
+        ans &= "姓名 : [林奕辰]    術科測試編號 : [1025311212]" & vbCrLf
+        ans &= "座號 : [12]         日期 : [2024/10/25]" & vbCrLf & vbCrLf
+        ans &= txt_01.Text & vbCrLf & vbCrLf
+        ans &= txt_02.Text & vbCrLf & vbCrLf
+        ans &= txt_03.Text & vbCrLf & vbCrLf
+        ans &= txt_04.Text & vbCrLf & vbCrLf
+        ans &= txt_05.Text & vbCrLf & vbCrLf
+        e.Graphics.DrawString(ans, Font, Brushes.Black, 100, 100)
+    End Sub
+End Class
