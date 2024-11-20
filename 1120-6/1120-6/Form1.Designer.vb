9046bb693fd296fceaf3ce9e5921a2b59426fe85
Thoomastvlife <153334866+Thoomastvlife@users.noreply.github.com>
2024-11-20 09:57:02 +0800

    Add files via upload

---
+++ 1120-6/1120-6/Form1.Designer.vb
@@ -0,0 +1,51 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        dgv = New DataGridView()
+        CType(dgv, ComponentModel.ISupportInitialize).BeginInit()
+        SuspendLayout()
+        ' 
+        ' dgv
+        ' 
+        dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        dgv.Location = New Point(12, 257)
+        dgv.Name = "dgv"
+        dgv.Size = New Size(502, 574)
+        dgv.TabIndex = 0
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(688, 843)
+        Controls.Add(dgv)
+        Name = "Form1"
+        Text = "Form1"
+        CType(dgv, ComponentModel.ISupportInitialize).EndInit()
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents dgv As DataGridView
+
+End Class
---
+++ 1120-6/1120-6/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Dim rno, ec, d(100, 4)
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Call rdata()
+        For i = 1 To rno
+            If ec = "" Then Call sp1(i)
+            If ec = "" Then Call sp2(i)
+            If ec = "" Then Call sp3(i)
+            d(i, 4) = ec
+        Next
+        Call wdata()
+    End Sub
+
+    Sub rdata()
+        FileOpen(1, "c:\test\1060306.sm", OpenMode.Input)
+        rno = 0
+        Do While Not EOF(1)
+            rno = rno + 1
+            For j = 1 To 3
+                Input(1, d(rno, j))
+            Next
+        Loop
+        FileClose(1)
+    End Sub
+
+    Sub sp1(i)
+
+    End Sub
+    Sub sp2(i)
+
+    End Sub
+
+    Sub sp3(i)
+
+    End Sub
+    Sub wdata()
+
+    End Sub
+
+End Class
