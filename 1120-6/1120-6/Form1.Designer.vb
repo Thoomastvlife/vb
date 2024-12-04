13a773f5eaa963f8967eea2b8b74b31f2d3bf705
Thoomastvlife <153334866+Thoomastvlife@users.noreply.github.com>
2024-12-04 09:31:57 +0800

    Add files via upload

--- 1120-6/1120-6/Form1.Designer.vb
+++ 1120-6/1120-6/Form1.Designer.vb
@@ -23,7 +23,17 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         dgv = New DataGridView()
+        GroupBox1 = New GroupBox()
+        TextBox4 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox1 = New TextBox()
+        Label4 = New Label()
+        Label3 = New Label()
+        Label2 = New Label()
+        Label1 = New Label()
         CType(dgv, ComponentModel.ISupportInitialize).BeginInit()
+        GroupBox1.SuspendLayout()
         SuspendLayout()
         ' 
         ' dgv
@@ -31,21 +41,114 @@ Partial Class Form1
         dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
         dgv.Location = New Point(12, 257)
         dgv.Name = "dgv"
-        dgv.Size = New Size(502, 574)
+        dgv.Size = New Size(566, 601)
         dgv.TabIndex = 0
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(TextBox4)
+        GroupBox1.Controls.Add(TextBox3)
+        GroupBox1.Controls.Add(TextBox2)
+        GroupBox1.Controls.Add(TextBox1)
+        GroupBox1.Controls.Add(Label4)
+        GroupBox1.Controls.Add(Label3)
+        GroupBox1.Controls.Add(Label2)
+        GroupBox1.Controls.Add(Label1)
+        GroupBox1.Location = New Point(12, 54)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(566, 197)
+        GroupBox1.TabIndex = 1
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "應檢人資料"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.Location = New Point(343, 111)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(100, 23)
+        TextBox4.TabIndex = 7
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(343, 36)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(100, 23)
+        TextBox3.TabIndex = 6
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(112, 108)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(100, 23)
+        TextBox2.TabIndex = 5
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(112, 36)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(100, 23)
+        TextBox1.TabIndex = 4
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(261, 111)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(73, 15)
+        Label4.TabIndex = 3
+        Label4.Text = "考  試  日  期"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(261, 39)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(79, 15)
+        Label3.TabIndex = 2
+        Label3.Text = "術科測試號碼"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(49, 111)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(31, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "座號"
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(49, 39)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(31, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "姓名"
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(688, 843)
+        ClientSize = New Size(612, 870)
+        Controls.Add(GroupBox1)
         Controls.Add(dgv)
         Name = "Form1"
-        Text = "Form1"
+        Text = "身分證號碼檢查"
         CType(dgv, ComponentModel.ISupportInitialize).EndInit()
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
         ResumeLayout(False)
     End Sub
 
     Friend WithEvents dgv As DataGridView
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
 
 End Class
